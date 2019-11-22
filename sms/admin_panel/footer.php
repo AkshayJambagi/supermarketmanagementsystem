3191f56aff6d8cccd1fd818407897fd28c8a642a
AkshayJambagi <akshayjambagi3@gmail.com>
2019-11-22 20:57:26 +0530

    Add files via upload

---
+++ sms/admin_panel/footer.php
@@ -0,0 +1,38 @@
+
+  <!-- Footer -->
+  <footer class="green row-padding padding-32">
+<div class="third">
+	<h3>Contact Us</h3>
+	<p>
+		<br><b>Butter Inventory </b>
+		<br>Dwarakanagar, BSK III Stage,
+		<br>Bangalore- Karataka - 560085.<br>
+		<br><b>Email : </b>butterinventory@gmail.com
+		<br><b>Contact Number: </b>(O) 080 080080080
+		<br><b>Fax – </b>080 080080080
+	</p>
+</div>
+
+
+  </footer>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidebar
+function sidebar_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+ 
+function sidebar_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+function liked() {
+	this.style.visibility="visible";
+}
+
+</script>
+
+
+</body></html>
---
+++ sms/admin_panel/header.php
@@ -0,0 +1,38 @@
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"><title>About Us - Butter Inventory</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../index_files/w3.css">
+<link rel="stylesheet" href="../sindex_files/css.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+.bar-block .bar-item {padding:20px}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+</style>
+</head><body>
+
+<!-- Sidebar (hidden by default) -->
+<nav class="sidebar bar-block card toper Inventory is Inventory Management System. xlarge animate-left" style="display:none;z-index:2;width:20%;min-width:300px" id="mySidebar">
+  <a class="bar-item button" onclick="sidebar_close()"><b><u><center>Catergories</center></u></b></a>
+    <a href="index.html" onclick="sidebar_close()" class="bar-item button">Home</a>
+  <a href="#" onclick="sidebar_close()" class="bar-item button">Login/Register</a>
+  <a href="form.html" onclick="sidebar_close()" class="bar-item button">Contact / About Us</a>
+  <a href="javascript:void(0)" onclick="sidebar_close()" class="bar-item button"><center>Close Menu</center></a>
+</nav>
+
+<!-- Top menu -->
+<div class=" green top">
+  <div class="xlarge" style="max-width:1200px;margin:auto">
+    <div class="button padding-16 left" onclick="sidebar_open()">☰</div>
+    <div class="center padding-16">Butter Inventory</div>
+  </div>
+</div>
